c283605ed01f2faddb0656fedba40205081dc337
Sum Yi MA <ma.xinyi@outlook.com>
2025-06-07 15:12:17 +0800

    god

--- app.py
+++ app.py
@@ -21,7 +21,18 @@ def evaluate_result(roll, skill):
 
 # Initialize Firebase
 if "firebase_initialized" not in st.session_state:
-    cred = credentials.Certificate("firebase_config.json")
+    cred = credentials.Certificate({
+        "type": st.secrets.firebase.type,
+        "project_id": st.secrets.firebase.project_id,
+        "private_key_id": st.secrets.firebase.private_key_id,
+        "private_key": st.secrets.firebase.private_key.replace('\\n', '\n'),
+        "client_email": st.secrets.firebase.client_email,
+        "client_id": st.secrets.firebase.client_id,
+        "auth_uri": st.secrets.firebase.auth_uri,
+        "token_uri": st.secrets.firebase.token_uri,
+        "auth_provider_x509_cert_url": st.secrets.firebase.auth_provider_x509_cert_url,
+        "client_x509_cert_url": st.secrets.firebase.client_x509_cert_url,
+    })
     firebase_admin.initialize_app(cred, {
         "databaseURL": "https://trpgdice-af12e-default-rtdb.asia-southeast1.firebasedatabase.app/"
     })
